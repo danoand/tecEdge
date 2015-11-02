32754c31f84dcceb3d459d184072a529eddae181
Dan Anderson <danandersonmobile@yahoo.com>
2015-11-02 16:04:23 -0600

    add metrics gathering

--- loaddata.go
+++ loaddata.go
@@ -3,6 +3,7 @@ package main
 import (
 	"io/ioutil"
 	"log"
+	"sync"
 
 	"github.com/BurntSushi/toml"
 )
@@ -10,12 +11,77 @@ import (
 var (
 	// Declare a variable to hold test emails and phone numbers
 	loadTestData testData
+
+	wrkerMetrics = make(map[string]jobData)
+	mutMetrics   = &sync.Mutex{}
+	wgMetrics    sync.WaitGroup
 )
 
 // Define a type to models the test data that will be loaded
 type testData struct {
 	Emails  []string
 	Numbers []string
+}
+
+// jobData defines a struct to hold the number of text and email tasks
+//   performed by a worker
+type jobData struct {
+	NumTexts  int
+	NumEmails int
+}
+
+// jobIncText increments the number of texts sent for the specified worker
+func jobIncText(inKey string) {
+	var tNumText int
+	var tNumEmails int
+	var tJobData jobData
+
+	// Lock wrkerMetrics
+	mutMetrics.Lock()
+
+	// Fetch the metrics data
+	tNumText = wrkerMetrics[inKey].NumTexts + 1
+	tNumEmails = wrkerMetrics[inKey].NumEmails
+
+	// Update new metrics value
+	tJobData.NumTexts = tNumText
+	tJobData.NumEmails = tNumEmails
+
+	// Assign updated metrics data to map
+	wrkerMetrics[inKey] = tJobData
+
+	// Release lock on wrkerMetrics
+	mutMetrics.Unlock()
+
+	wgMetrics.Done()
+	return
+}
+
+// jobIncEmail increments the number of emails sent for the specified worker
+func jobIncEmail(inKey string) {
+	var tNumText int
+	var tNumEmails int
+	var tJobData jobData
+
+	// Lock wrkerMetrics
+	mutMetrics.Lock()
+
+	// Fetch the metrics data
+	tNumText = wrkerMetrics[inKey].NumTexts
+	tNumEmails = wrkerMetrics[inKey].NumEmails + 1
+
+	// Update new metrics value
+	tJobData.NumTexts = tNumText
+	tJobData.NumEmails = tNumEmails
+
+	// Assign updated metrics data to map
+	wrkerMetrics[inKey] = tJobData
+
+	// Release lock on wrkerMetrics
+	mutMetrics.Unlock()
+
+	wgMetrics.Done()
+	return
 }
 
 // Function loadRequests loads the requests data structures
--- main.go
+++ main.go
@@ -11,30 +11,40 @@ import (
 var (
 	// Declare a function that processes a "texting" task
 	exText = func(to string, text string, wrkr string) {
+		// Execute the task
 		fmt.Printf("\n\nSending Text Message to: %v\nMessage -> %v\n---\nProcessed by worker: %v\n\n", to, text, wrkr)
+
+		// Increment metrics for the specified worker
+		wgMetrics.Add(1)
+		go jobIncText(wrkr)
 	}
 
 	// Declare a function that processes an "emailing" task
 	exEmail = func(to string, body string, wrkr string) {
+		// Execute the task
 		fmt.Printf("\n\nSending Email Message to: %v\nMessage -> %v\n---\nProcessed by worker: %v\n\n", to, body, wrkr)
+
+		// Increment metrics for the specified worker
+		wgMetrics.Add(1)
+		go jobIncEmail(wrkr)
 	}
 )
 
-// inItem defines a struct type that will describe a job to be processed
-type inItem struct {
-	Task func(string, string, string)
-	To   string
-	Text string
+// inJob defines a struct type that will describe a job to be processed
+type inJob struct {
+	Task func(string, string, string) // Task to be executed in this job
+	To   string                       // The recipient of the outbound message (phone number or email address)
+	Text string                       // The outbound message intended for the recipient (text or email)
 }
 
-// procJobs works on a channel (or queue) of jobs
-func procJob(jobs <-chan inItem, idWrker string) {
-	for job := range jobs {
+// procjobQueue is a function that "runs" on each worker processing jobQueue
+func procJob(jobQueue <-chan inJob, idWrker string) {
+	for job := range jobQueue {
 		// Execute the task referred to by the inbound job
 		job.Task(job.To, job.Text, idWrker)
 
 		// Wait for an elapse of time
-		time.Sleep(3 * time.Second)
+		time.Sleep(1 * time.Second)
 	}
 }
 
@@ -45,8 +55,8 @@ func main() {
 		log.Fatal("ERROR: loading the testing data.  Stopping.")
 	}
 
-	// Create a channel (queue) that will contain jobs to be executed
-	jobs := make(chan inItem)
+	// Create a channel (queue) that will contain jobQueue to be executed
+	jobQueue := make(chan inJob)
 
 	// Create a Waitgroup that indicates if workers are still running
 	var wg sync.WaitGroup
@@ -55,53 +65,63 @@ func main() {
 	for i := 0; i < 3; i++ {
 
 		// Create a function that serves as a worker and assign to a variable
-		workerWrapper := func(fnName string, jobs <-chan inItem) {
-			wrkerName := fmt.Sprintf("WRK %v", fnName)
-			fmt.Printf("Starting a worker: %v\n", wrkerName)
+		workerWrapper := func(fnName string, jobQueue <-chan inJob) {
+			wrkerName := fmt.Sprintf("%v", fnName)
+			fmt.Printf("WORKER: Starting a worker: %v\n", wrkerName)
 			defer wg.Done()
-			procJob(jobs, wrkerName)
-			fmt.Printf("Worker %v is done.\n", wrkerName)
+			procJob(jobQueue, wrkerName)
+			fmt.Printf("WORKER: Worker %v is done.\n", wrkerName)
 		}
 
 		wg.Add(1)
 
 		// Spin up a worker function
-		go workerWrapper(fmt.Sprintf("%p", &workerWrapper), jobs)
+		go workerWrapper(fmt.Sprintf("%p", &workerWrapper), jobQueue)
+		// Add an entry to wrkerMetrics to gather metrics on the worker
+		wrkerMetrics[fmt.Sprintf("%p", &workerWrapper)] = jobData{0, 0}
 	}
 
-	var procItem inItem
+	var procJob inJob
 
-	// Create a bunch of jobs
+	// Create a bunch of jobQueue
 	for i := 0; i < 25; i++ {
 
-		// Create a random integer to generate a distribution of jobs
+		// Create a random integer to generate a distribution of jobQueue
 		myDist := rand.Intn(100)
 		switch {
 		case myDist <= 70:
-			// Tend to create more text messages jobs than email jobs
-			tmpItem := inItem{
+			// Tend to create more text messages jobQueue than email jobQueue
+			tmpItem := inJob{
 				Task: exText,
 				To:   loadTestData.Numbers[myDist],
 				Text: fmt.Sprintf("This is my text to: %v", loadTestData.Numbers[myDist]),
 			}
-			procItem = tmpItem
+			procJob = tmpItem
 		default:
-			tmpItem := inItem{
+			tmpItem := inJob{
 				Task: exEmail,
 				To:   loadTestData.Emails[myDist],
 				Text: fmt.Sprintf("This is my email to: %v", loadTestData.Emails[myDist]),
 			}
-			procItem = tmpItem
+			procJob = tmpItem
 		}
 
 		// Add a job to the job queue/channel
-		jobs <- procItem
+		jobQueue <- procJob
 	}
 
-	// Close the "jobs" channel (queue)
-	close(jobs)
-	fmt.Println("Closed jobs!")
+	// Close the "jobQueue" channel (queue)
+	close(jobQueue)
+	fmt.Println("Closed jobQueue!")
 
 	// Wait until the workers complete
 	wg.Wait()
+
+	// Print out metrics to the log
+	fmt.Printf("\n\n*********** PRINT OUT METRICS FOR EACH WORKER *************\n")
+	// Range through the metrics data
+	for k, v := range wrkerMetrics {
+		fmt.Printf("WORKER %v processed %v texts & %v emails\n", k, v.NumTexts, v.NumEmails)
+	}
+	fmt.Printf("************************\n\n")
 }
